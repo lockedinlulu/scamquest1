4afd85ddf46b98cc8f4c2627d4c0b079b8f5c76c
Gatsdaporn Sayananon <yunastras@gmail.com>
2026-04-21 11:59:21 -0400

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "export const GROQ_API_KEY="$GROQ_API_KEY"; export const TAVILY_API_KEY="$TAVILY_API_KEY"; export const ELEVENLABS_API_KEY="$ELEVENLABS_API_KEY";" > config.js
+cat > config.js << EOF
+export const GROQ_API_KEY = "$GROQ_API_KEY";
+export const TAVILY_API_KEY = "$TAVILY_API_KEY";
+export const ELEVENLABS_API_KEY = "$ELEVENLABS_API_KEY";
+EOF
